5ebd4145ff9cae28825dd38760f2695ee46d922b
Neeraj gaur <neerajgaur8448@gmail.com>
2024-10-22 15:42:34 +0530

    refactor: Update API base URL to use secure connection

--- frontend/src/socketContext/SocketContext.jsx
+++ frontend/src/socketContext/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (userInfo) {
-      const newSocket = io("http://localhost:8000", {
+      const newSocket = io("https://twitter-clone-b1qc.onrender.com", {
         query: {
           userId: userInfo._id,
         },
